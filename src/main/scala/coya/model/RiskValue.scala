27b8fa5dd5d7f49361e0b1f217243591ecb22cf4
Dmytro Orlov <orlov.dm@gmail.com>
2018-12-02 12:36:09 +0100

    Processor: improve readability

--- src/main/scala/coya/model/RiskValue.scala
+++ src/main/scala/coya/model/RiskValue.scala
@@ -1,3 +1,5 @@
 package coya.model
 
 case class RiskValue(value: Int) extends AnyVal
+
+case class Surcharge(value: BigDecimal) extends AnyVal
--- src/main/scala/coya/quote/Processor.scala
+++ src/main/scala/coya/quote/Processor.scala
@@ -1,7 +1,7 @@
 package coya.quote
 
-import cats.Semigroup
-import cats.data.Kleisli
+import cats.data.{Kleisli, Reader}
+import cats.instances.all._
 import cats.syntax.option._
 import coya.model._
 import squants.market._
@@ -12,67 +12,76 @@ trait Processor {
 
 object CoyaProcessor extends Processor {
   def priceFor(user: User, products: Seq[Product]): Option[Money] = {
-    val basePremiumValue: Kleisli[Option, Product, Double] = Kleisli {
-      case _: House => 0.03.some
-      case _: Banana => 1.15.some
-      case _: Bicycle => 0.10.some
-      case _: Helicopter => 0.05.some
+    val baseSurcharge: Reader[Product, Surcharge] = Reader {
+      case _: House => Surcharge(BigDecimal("0.03"))
+      case _: Banana => Surcharge(BigDecimal("1.15"))
+      case _: Bicycle => Surcharge(BigDecimal("0.10"))
+      case _: Helicopter => Surcharge(BigDecimal("0.05"))
     }
 
-    def productSubtotal(p: Product) = for {
-      base <- basePremiumValue(p)
-    } yield p.value * base
-
-    val productSurcharge: Kleisli[Option, Product, Double] = Kleisli {
+    val productSurcharge: Kleisli[Option, Product, Surcharge] = Kleisli {
       case h: House if h.size < 30 || h.size > 1000 => none
-      case h: House if h.address.locationRisk.value < 100 => 0.7.some
-      case h: House if h.address.locationRisk.value < 300 => 1.0.some
-      case h: House if h.address.locationRisk.value <= 501 => 2.5.some
+      case h: House if h.address.locationRisk.value < 100 => Surcharge(BigDecimal("0.7")).some
+      case h: House if h.address.locationRisk.value < 300 => Surcharge(BigDecimal("1.0")).some
+      case h: House if h.address.locationRisk.value <= 501 => Surcharge(BigDecimal("2.5")).some
       case h: House if h.address.locationRisk.value > 501 => none
       case b: Banana if b.blackSpots < 3 || b.blackSpots > 12 => none
-      case bc: Bicycle => (bc.gears * 0.08).some
-      case _ => 1.0.some
+      case bc: Bicycle => Surcharge(bc.gears * BigDecimal("0.08")).some
+      case _ => Surcharge(BigDecimal("1.0")).some
     }
 
-    def productPrice(p: Product) = for {
-      sub <- productSubtotal(p)
-      ps <- productSurcharge(p)
-    } yield sub * ps
-
-    def applyProductPrice(u: User, ps: Seq[Product]) = {
-      lazy val expensiveHouseExists = ps.exists {
-        case h: House if h.value ># EUR(10000000) => true
-        case _ => false
-      }
-      ps.map {
-        case h: House if expensiveHouseExists => productPrice(h).map(_ * 1.15)
-        case _: Banana if u.risk.value > 200 => none
-        case p => productPrice(p)
-      }
-    }
-
-    val userSurcharge: Kleisli[Option, User, Double] = Kleisli(_.risk match {
-      case low if low.value <= 20 => 0.3.some
-      case mid if mid.value <= 200 => 1.0.some
-      case hi if hi.value <= 500 => 3.0.some
+    val userSurcharge: Kleisli[Option, User, Surcharge] = Kleisli(_.risk match {
+      case low if low.value <= 20 => Surcharge(BigDecimal("0.3")).some
+      case mid if mid.value <= 200 => Surcharge(BigDecimal("1.0")).some
+      case hi if hi.value <= 500 => Surcharge(BigDecimal("3.0")).some
       case _ => none
     })
 
-    for {
-      us <- userSurcharge(user)
-      total <- Semigroup.combineAllOption(applyProductPrice(user, products)).flatten
-      if !(products.exists {
-        case _: Bicycle => true
+    val productSurcharges = {
+      lazy val expensiveHouseExists = products.exists {
+        case h: House if h.value ># EUR(10000000) => true
         case _ => false
-      } && user.risk.value >= 150 && total > EUR(100))
-    } yield us * total
-  }
+      }
+      val expensiveHouseSurcharge: Kleisli[Option, Product, Surcharge] = Kleisli {
+        case _: House if expensiveHouseExists => Surcharge(BigDecimal("1.15")).some
+        case _: Banana if user.risk.value > 200 => none
+        case _ => Surcharge(BigDecimal("1.0")).some
+      }
+      for {
+        p <- products
+        u <- userSurcharge(user)
+        surcharge = Seq(productSurcharge, expensiveHouseSurcharge, baseSurcharge.lift[Option])
+          .reduce((a, b) => for {
+            as <- a
+            bs <- b
+          } yield Surcharge(as.value * bs.value))
+      } yield surcharge(p).map(_.value * u.value)
+    }
 
-  implicit val SemigroupMoney: Semigroup[Option[Money]] =
-    (x: Option[Money], y: Option[Money]) => for {
+    val prices = productSurcharges
+      .zip(products)
+      .map {
+        case (s, p) => s.map(p.value * _)
+      }
+
+    val total = prices.reduce((x, y) => for {
       i <- x
       j <- y
-    } yield i + j
+    } yield i + j)
+
+    def riskyBicycle(total: Money) = {
+      def bicycleExists(ps: Seq[Product]) = ps.exists {
+        case _: Bicycle => true
+        case _ => false
+      }
+
+      bicycleExists(products) && user.risk.value >= 150 && total > EUR(100)
+    }
+
+    for {
+      t <- total if !riskyBicycle(t)
+    } yield t
+  }
 
   implicit val EurMoneyContext: MoneyContext = MoneyContext(EUR, defaultCurrencySet, Nil)
 }
